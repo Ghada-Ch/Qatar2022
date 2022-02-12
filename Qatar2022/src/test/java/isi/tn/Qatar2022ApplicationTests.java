7675adf49695328338b48a6452d2151b415bb012
Ghada-Ch <57904143+Ghada-Ch@users.noreply.github.com>
2022-02-12 20:54:16 +0100

    Add files via upload

---
+++ Qatar2022/src/test/java/isi/tn/Qatar2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package isi.tn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qatar2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
